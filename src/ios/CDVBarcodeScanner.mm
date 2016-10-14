f82ab8bb86ca05cbf84ec4ad3786608be753b22b
Kyle Alderman <kyle.alderman@rockfishinteractive.com>
2016-10-14 15:17:17 -0500

    forcing app to load alternate nib

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -289,7 +289,7 @@ parentViewController:(UIViewController*)parentViewController
     self.plugin               = plugin;
     self.callback             = callback;
     self.parentViewController = parentViewController;
-    self.alternateXib         = alternateXib;
+    self.alternateXib         = @"scannerOverlay";
 
     self.is1D      = YES;
     self.is2D      = YES;
